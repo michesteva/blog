ddd5d7ad6ae3a00821ff90dcae73cdcc0cbaafa4
Carlos Matallín <matallo@cartodb.com>
2014-08-06 22:43:06 +0200

    fix feed, missing from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -256,6 +256,7 @@ module.exports = function(grunt) {
           dest: '<%= config.dist %>',
           src: [
             '*.{ico,png,txt}',
+            'feed.*',
             '**/*.html',
             'img/**/*.{gif,jpeg,jpg,png}'
           ]
